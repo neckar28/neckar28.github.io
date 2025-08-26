fcab837918fdb744a50e1fd93a83af51170fac06
neckar28 <nini43500@gmail.com>
2025-08-26 21:47:31 +0900

    Build _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "카테고리",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/categories/",
+        "teaser": null
+      },{
+        "title": "포트폴리오",
+        "excerpt":"포트폴리오 정리 공간  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/",
+        "teaser": null
+      },{
+        "title": "왜 파이썬이었나?",
+        "excerpt":"Robospital은 프로그래밍 교육용 프로그램으로, 프로그래밍 언어로 Python을 채택했다. 왜 파이썬을 선택했을까? https://github.blog/news-insights/octoverse/octoverse-2024/ Robospital을 처음 기획하던 2020년 기준으로, Python은 가장 많이 사용되는 프로그래밍 언어는 아니었다. 하지만 몇 가지 이유로 Python을 채택 했다. Python을 활용해 본 경험이 있어서, 프로그래밍 문제를 만들거나 구현에 적합했다. 이 전에 대학원 연구실에 다닐 때 Python을 활용해 본...","categories": ["기획"],
+        "tags": ["Python","Robospital","기획"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/05/%EC%99%9C-%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%9D%B4%EC%97%88%EB%82%98.html",
+        "teaser": null
+      },{
+        "title": "Robospital의 프로그래밍 문제와 학습과정",
+        "excerpt":"프로그래밍 퍼즐 구성은 Robospital 기획의 핵심적인 파트이다. 퍼즐은 교육용으로 제작되어야 했기 때문에 몇 가지 원칙을 가지고 프로그램을 구성했다. 원칙들은 다음과 같다. 따라 쓰기에서 시작해서 응용으로 끝낸다. 가급적이면 한 문제에 새로운 요소를 하나 넣는다. 어떤 문제에서 어떤 지식을 선행하여 알고 있어야 하는지 확인해야한다. 응용문제는 실생활의 언어를 사용하여, 사용자가 학습에 효능감을 얻도록...","categories": ["기획"],
+        "tags": ["Python","Robospital","기획"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/05/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EB%AC%B8%EC%A0%9C%EC%99%80-%ED%95%99%EC%8A%B5%EA%B3%BC%EC%A0%95.html",
+        "teaser": null
+      }]
