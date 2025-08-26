ad9eec675ceab5fafa5ad46693544d156131a735
neckar28 <nini43500@gmail.com>
2025-08-27 02:19:25 +0900

    Fix gitignore and Add _site directory

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "카테고리",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/categories/",
+        "teaser": null
+      },{
+        "title": "포트폴리오",
+        "excerpt":"포트폴리오 정리 공간  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/",
+        "teaser": null
+      },{
+        "title": "왜 파이썬이었나?",
+        "excerpt":"Robospital은 프로그래밍 교육용 프로그램으로, 프로그래밍 언어로 Python을 채택했다. 왜 파이썬을 선택했을까? https://github.blog/news-insights/octoverse/octoverse-2024/ Robospital을 처음 기획하던 2020년 기준으로, Python은 가장 많이 사용되는 프로그래밍 언어는 아니었다. 하지만 몇 가지 이유로 Python을 채택 했다. Python을 활용해 본 경험이 있어서, 프로그래밍 문제를 만들거나 구현에 적합했다. 이 전에 대학원 연구실에 다닐 때 Python을 활용해 본...","categories": ["기획"],
+        "tags": ["Python","Robospital"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/05/%EC%99%9C-%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%9D%B4%EC%97%88%EB%82%98.html",
+        "teaser": null
+      },{
+        "title": "Robospital의 프로그래밍 문제와 학습과정",
+        "excerpt":"프로그래밍 퍼즐 구성은 Robospital 기획의 핵심적인 파트이다. 퍼즐은 교육용으로 제작되어야 했기 때문에 몇 가지 원칙을 가지고 프로그램을 구성했다. 원칙들은 다음과 같다. 따라 쓰기에서 시작해서 응용으로 끝낸다. 가급적이면 한 문제에 새로운 요소를 하나 넣는다. 어떤 문제에서 어떤 지식을 선행하여 알고 있어야 하는지 확인해야한다. 응용문제는 실생활의 언어를 사용하여, 사용자가 학습에 효능감을 얻도록...","categories": ["기획"],
+        "tags": ["Python","Robospital"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/05/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EB%AC%B8%EC%A0%9C%EC%99%80-%ED%95%99%EC%8A%B5%EA%B3%BC%EC%A0%95.html",
+        "teaser": null
+      },{
+        "title": "육성 시뮬레이션 구조, 프린세스 메이커와 우마무스메 비교",
+        "excerpt":"※ 2022년 6월 기준 자료를 재구성했습니다. 육성 시뮬레이션 육성 시뮬레이션 게임은 아이, 애완동물, 유명인 등을 기르는 과정을 흉내 낸 생활 시뮬레이션 게임으로, 일반적인 RPG 게임의 육성 요소를 부각한 게임이라고 할 수 있다. 프린세스 메이커나, 다마고치, 우마무스메 등의 게임이 육성 시뮬레이션에 해당한다. 육성 시뮬레이션의 목표는 정해진 시간 안에 스탯을 키워서 엔딩을...","categories": ["기획"],
+        "tags": ["공주님을부탁해!"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/06/%EC%9C%A1%EC%84%B1-%EC%8B%9C%EB%AE%AC%EB%A0%88%EC%9D%B4%EC%85%98-%EA%B5%AC%EC%A1%B0%EC%99%80-%EB%B9%84%EA%B5%90.html",
+        "teaser": null
+      },{
+        "title": "코드 에디터 UI 기획",
+        "excerpt":"Robospital 코드 에디터 UI Visual Studio 2022의 인터페이스 갓 대학에 들어갔을 때, 첫 IDE로 Visual Studio 2008을 썼었다. 흰 바탕에 깔끔한 IDE 였고 대부분의 IDE가 비슷하게 복잡하긴 하지만, 처음 프로그래밍을 접하는 사람에겐 익히기 쉽지 않았다. Robospital은 초보자들이 쉽게 프로그래밍에 입문할 수 있도록 도움을 주는 프로그램이다. 따라서 UI를 기획할 때도 IDE를...","categories": ["기획"],
+        "tags": ["Robospital"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/08/06/%EC%BD%94%EB%93%9C-%EC%97%90%EB%94%94%ED%84%B0-UI-%EA%B8%B0%ED%9A%8D.html",
+        "teaser": null
+      },{
+        "title": "Unity-Python Integration",
+        "excerpt":"기획 단계에서 Robospital을 Python 교육용 게임으로, Unity3D를 이용해 만들기로 결정했다. 따라서 사용자가 게임 안에서 코드를 작성하고, 그 코드를 곧바로 실행할 방법이 필요했다. 또한 warning이나 syntax error 하이라이팅 정보를 받아, 게임 화면에 시각적 피드백을 남길 수 있다면 초보자에게 더 직관적인 피드백을 줄 수 있을 것 같았다. 따라서 Python 코드를 문자열로 받아...","categories": ["프로그래밍"],
+        "tags": ["C#","Python","Robospital","Unity3D"],
+        "url": "/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/2025/08/07/Unity-Python-Integration.html",
+        "teaser": null
+      }]
