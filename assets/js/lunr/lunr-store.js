1c85c0c119672b234c5222514fbf2935a44dd91e
neckar28 <nini43500@gmail.com>
2025-09-04 02:41:54 +0900

    Update site

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -40,4 +40,10 @@ var store = [{
         "tags": ["C#","Python","Robospital","Unity3D"],
         "url": "/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/2025/08/07/Unity-Python-Integration.html",
         "teaser": null
+      },{
+        "title": "스쿼트+미소녀 게임 기획 및 MVP 구현",
+        "excerpt":"기획 구직 스트레스에 시달리던 2025년 8월 27일, 게임을 만들어야겠다고 결심한다. 아이디어 도출 새로운 게임에 대한 아이디어는 항상 경험해본 게임, 좋아하는 요소들을 늘어놓는 데서 시작한다. 내가 좋아하는 게임 요소들을 티어로 나눠보면 다음과 같다. Tier 1 미소녀 애니메이션 스타일 팬서비스 공장 자동화 프로그래밍 Tier 2 VR 추리 귀여움 시뮬레이션 밝은 분위기 샌드박스...","categories": ["기획"],
+        "tags": ["C#","Unity3D","VR"],
+        "url": "/%EA%B8%B0%ED%9A%8D/2025/09/03/%EC%8A%A4%EC%BF%BC%ED%8A%B8+%EB%AF%B8%EC%86%8C%EB%85%80-%EA%B2%8C%EC%9E%84-%EA%B8%B0%ED%9A%8D-%EB%B0%8F-MVP-%EA%B5%AC%ED%98%84.html",
+        "teaser": null
       }]
